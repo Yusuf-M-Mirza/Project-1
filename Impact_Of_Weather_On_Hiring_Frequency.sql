d88377f5f4df64c1a18605329a64f8c8e86e6848
Yusuf <ymirzayas@gmail.com>
2025-07-03 18:03:39 +0100

    Add files via upload

---
+++ Impact_Of_Weather_On_Hiring_Frequency.sql
@@ -0,0 +1,22 @@
+-- Query to provide data on number of bikes hired, mean temperature and total rainfall for all 31 days
+SELECT
+	w.Day,
+    w.Temperature,
+    w.Rainfall,
+    j.Total_Rides/100 AS Total_Rides
+FROM
+	(SELECT
+		DAY(DATE) AS Day,
+		TG AS Temperature,
+        RR AS Rainfall
+	FROM london_bikes.weather
+	WHERE YEAR(DATE)=2023 AND MONTH(DATE)=8 AND DAYOFWEEK(DATE) IN (1,2,3,4,5,6,7)
+	ORDER BY DAY) AS w
+    LEFT JOIN
+    (SELECT
+		DAY(start_date) AS Day,
+		COUNT(start_date) AS Total_Rides
+	FROM london_bikes.journeys
+    WHERE DAYOFWEEK(start_date) IN (1,2,3,4,5,6,7)
+	GROUP BY Day) AS j
+    ON w.Day=j.Day
